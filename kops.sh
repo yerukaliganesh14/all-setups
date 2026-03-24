bbc61217c6d7550887c87c4a4b9d3a532371a898
yerukaliganesh14 <yerukaliganesh14@gmail.com>
2026-03-24 09:37:29 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,7 +1,3 @@
-#vim .bashrc
-#export PATH=$PATH:/usr/local/bin/
-#source .bashrc
-
 #! /bin/bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
