b1dd4bef5b6063bc4a81248b4237ab484c134bfc
yerukaliganesh14 <yerukaliganesh14@gmail.com>
2026-03-24 09:27:11 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -10,8 +10,8 @@ sudo mv kops /usr/local/bin/kops
 chmod +x kubectl
 mv kubectl /usr/local/bin/kubectl
 
-aws s3api create-bucket --bucket mustafa.project.flm.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket mustafa.project.flm.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://mustafa.project.flm.k8s.local
-kops create cluster --name mustafa.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.large --master-volume-size=30 --node-count=3 --node-size t2.medium --node-volume-size=20
-kops update cluster --name mustafa.k8s.local --yes --admin
+aws s3api create-bucket --bucket ganesh12.flm --region us-east-1
+aws s3api put-bucket-versioning --bucket ganesh12.flm --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://ganesh12.flm
+kops create cluster --name ganeshcc.k8s.local --zones us-east-1a,us-east-1b --master-count=1 --master-size t2.large --master-volume-size=30 --node-count=2 --node-size t2.medium --node-volume-size=20
+kops update cluster --name ganeshcc.k8s.local --yes --admin
