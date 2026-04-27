a877c76c32cb555ab27346b2d385d73a80db2f79
yerukaliganesh14 <yerukaliganesh14@gmail.com>
2026-04-27 12:14:01 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
-sudo yum update –y
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
